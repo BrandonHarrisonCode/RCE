a1c77e4e2a900a68b20a694a7d39f5eb87cf9484
Brandon Harrison <brandonharrisoncode@gmail.com>
2023-08-15 13:50:07 -0600

    Add tests to king

--- src/board/piece/king.rs
+++ src/board/piece/king.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct King;
 
 const WHITE_SYMBOL: &str = "♚";
@@ -18,5 +18,86 @@ impl Piece for King {
 
     fn get_moveset(_square: &Square) -> Vec<Move> {
         Vec::new()
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_king_derived_traits() {
+        let piece = King {};
+        dbg!(&piece);
+
+        assert_eq!(piece, piece.clone());
+    }
+
+    #[test]
+    fn test_king_display_white() {
+        let output = super::WHITE_SYMBOL;
+        let correct = "♚";
+
+        assert_eq!(output, correct);
+    }
+
+    #[test]
+    fn test_king_display_black() {
+        let output = super::BLACK_SYMBOL;
+        let correct = "♔";
+
+        assert_eq!(output, correct);
+    }
+
+    #[test]
+    fn test_king_get_piece_symbol_white() {
+        let piece = PieceKind::King(Color::White);
+        let correct = "♚";
+
+        assert_eq!(piece.get_piece_symbol(), correct);
+    }
+
+    #[test]
+    fn test_king_get_piece_symbol_black() {
+        let piece = PieceKind::King(Color::Black);
+        let correct = "♔";
+
+        assert_eq!(piece.get_piece_symbol(), correct);
+    }
+
+    #[test]
+    fn test_king_eq() {
+        let left = PieceKind::King(Color::White);
+        let right = PieceKind::King(Color::White);
+
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn test_king_neq() {
+        let left = PieceKind::King(Color::White);
+        let right = PieceKind::King(Color::Black);
+
+        assert_ne!(left, right);
+    }
+
+    #[test]
+    fn test_king_neq_rev() {
+        // Test if addition is commutative
+        let right = PieceKind::King(Color::White);
+        let left = PieceKind::King(Color::Black);
+
+        assert_ne!(left, right);
+    }
+
+    #[test]
+    fn test_king_get_moveset() {
+        let piece = PieceKind::King(Color::White);
+        let result = piece.get_moveset(&Square::new(0, 0));
+        let correct = Vec::new();
+
+        assert_eq!(result, correct);
     }
 }
