b227d645969cd31373b5284e446bbb7d012b666b
Brandon Harrison <brandonharrisoncode@gmail.com>
2023-08-16 18:21:02 -0600

    Add tests for Ply and Square

--- src/board/ply/mod.rs
+++ src/board/ply/mod.rs
@@ -13,3 +13,32 @@ impl fmt::Display for Ply {
         write!(f, "{} -> {}", self.start, self.dest)
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_derived_traits() {
+        let start = Square::new(3, 5);
+        let dest = Square::new(5, 4);
+        let ply = Ply::new(start, dest);
+        dbg!(&ply);
+
+        assert_eq!(ply, ply.clone());
+    }
+
+    #[test]
+    fn test_display() {
+        let start = Square::new(3, 5);
+        let dest = Square::new(5, 4);
+        let ply = Ply::new(start, dest);
+
+        let result = ply.to_string();
+        let correct = String::from(format!("{} -> {}", start, dest));
+
+        assert_eq!(result, correct);
+    }
+}
