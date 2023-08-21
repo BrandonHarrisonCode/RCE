bf50478b6d97f0adcff69485f65d94a4ab435b9c
Brandon Harrison <brandonharrisoncode@gmail.com>
2023-08-21 12:48:38 -0600

    Add tests for pieces

--- src/board/piece/mod.rs
+++ src/board/piece/mod.rs
@@ -102,6 +102,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_derived_traits_piece() {
+        let piece = PieceKind::Pawn(Color::White);
+        dbg!(&piece);
+
+        assert_eq!(piece, piece.clone());
+    }
+
+    #[test]
     fn display() {
         PieceKind::Pawn(Color::White).to_string();
         PieceKind::King(Color::White).to_string();
@@ -116,6 +124,23 @@ mod tests {
         PieceKind::Rook(Color::Black).to_string();
         PieceKind::Bishop(Color::Black).to_string();
         PieceKind::Knight(Color::Black).to_string();
+    }
+
+    #[test]
+    fn test_get_color() {
+        assert_eq!(PieceKind::Pawn(Color::White).get_color(), Color::White);
+        assert_eq!(PieceKind::King(Color::White).get_color(), Color::White);
+        assert_eq!(PieceKind::Queen(Color::White).get_color(), Color::White);
+        assert_eq!(PieceKind::Rook(Color::White).get_color(), Color::White);
+        assert_eq!(PieceKind::Bishop(Color::White).get_color(), Color::White);
+        assert_eq!(PieceKind::Knight(Color::White).get_color(), Color::White);
+
+        assert_eq!(PieceKind::Pawn(Color::Black).get_color(), Color::Black);
+        assert_eq!(PieceKind::King(Color::Black).get_color(), Color::Black);
+        assert_eq!(PieceKind::Queen(Color::Black).get_color(), Color::Black);
+        assert_eq!(PieceKind::Rook(Color::Black).get_color(), Color::Black);
+        assert_eq!(PieceKind::Bishop(Color::Black).get_color(), Color::Black);
+        assert_eq!(PieceKind::Knight(Color::Black).get_color(), Color::Black);
     }
 
     #[test]
@@ -144,5 +169,27 @@ mod tests {
         let piece1 = PieceKind::Pawn(Color::White);
         let piece2 = PieceKind::Queen(Color::Black);
         assert_ne!(piece1, piece2);
+    }
+
+    #[test]
+    fn test_get_all_legal_moves() {
+        let piece = PieceKind::Pawn(Color::White);
+        let start_square = Square::new(5, 7);
+        let dest_square = Square::new(6, 7);
+
+        let result = piece.get_all_legal_moves(&start_square);
+        let mut correct = Vec::new();
+        correct.push(Ply::new(start_square, dest_square));
+
+        assert_eq!(result, correct);
+    }
+
+    #[test]
+    fn test_derived_traits_color() {
+        let color = Color::White;
+        dbg!(&color);
+        println!("{}", color);
+
+        assert_eq!(color, color.clone());
     }
 }
