15b4a1c65f863368b940a88510ddc104488c1b6d
Brandon Harrison <brandonharrisoncode@gmail.com>
2024-01-03 22:10:53 -0700

    Add tests for board builder

--- src/board/boardbuilder.rs
+++ src/board/boardbuilder.rs
@@ -56,7 +56,7 @@ impl BoardBuilder {
 
 
     #[allow(dead_code)]
-    pub fn turn(mut self, white: bool) -> BoardBuilder {
+    pub fn white_turn(mut self, white: bool) -> BoardBuilder {
         self.is_white_turn = white;
         self
     }
@@ -134,12 +134,12 @@ impl BoardBuilder {
     }
 
     #[allow(dead_code)]
-    pub fn history(mut self, history: Vec<Ply>) -> BoardBuilder {
-        self.history = history;
+    pub fn history(mut self, history: &Vec<Ply>) -> BoardBuilder {
+        self.history = history.clone();
         self
     }
 
-    /// Returns a new board given a FEN string
+    /// Rewhite_turns a new board given a FEN string
     ///
     /// # Arguments
     ///
@@ -305,6 +305,7 @@ impl BoardBuilder {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::super::square::Square;
 
     #[test]
     fn from_fen_starting_position() {
@@ -368,5 +369,412 @@ mod tests {
         };
 
         assert_eq!(Board::new().from_fen(fen), correct);
+    }
+
+    #[test]
+    fn board_builder_default() {
+        let board = BoardBuilder::default().build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_black_white_turn() {
+        let board = BoardBuilder::default().white_turn(false).build();
+        let correct = Board {
+            is_white_turn: false,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_white_turn() {
+        let board = BoardBuilder::default().white_turn(false).white_turn(true).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_white_kingside_castling() {
+        let board = BoardBuilder::default().kingside_castling(true, false).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: false,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_black_kingside_castling() {
+        let board = BoardBuilder::default().kingside_castling(false, false).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: false,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_white_queenside_castling() {
+        let board = BoardBuilder::default().queenside_castling(true, false).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: false,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_black_queenside_castling() {
+        let board = BoardBuilder::default().queenside_castling(false, false).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: false,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_pawns() {
+        let board = BoardBuilder::default().pawns(true, 1).pawns(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 1,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 2,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_king() {
+        let board = BoardBuilder::default().king(true, 1).king(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 1,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 2,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_queens() {
+        let board = BoardBuilder::default().queens(true, 1).queens(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 1,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 2,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_rooks() {
+        let board = BoardBuilder::default().rooks(true, 1).rooks(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 1,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 2,
+            b_bishops: 0,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_bishops() {
+        let board = BoardBuilder::default().bishops(true, 1).bishops(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 1,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 2,
+            b_knights: 0,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_knights() {
+        let board = BoardBuilder::default().knights(true, 1).knights(false, 2).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 1,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 2,
+            history: Vec::new(),
+        };
+
+        assert_eq!(board, correct);
+    }
+
+    #[test]
+    fn board_builder_history() {
+        let history = vec![Ply::new(Square::new("a1"), Square::new("a2"))];
+        let board = BoardBuilder::default().history(&history).build();
+        let correct = Board {
+            is_white_turn: true,
+
+            w_kingside_castling: true,
+            w_queenside_castling: true,
+            b_kingside_castling: true,
+            b_queenside_castling: true,
+
+            w_pawns: 0,
+            w_king: 0,
+            w_queens: 0,
+            w_rooks: 0,
+            w_bishops: 0,
+            w_knights: 0,
+            b_pawns: 0,
+            b_king: 0,
+            b_queens: 0,
+            b_rooks: 0,
+            b_bishops: 0,
+            b_knights: 0,
+            history: history,
+        };
+
+        assert_eq!(board, correct);
     }
 }
