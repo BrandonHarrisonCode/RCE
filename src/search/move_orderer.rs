f6d243837b6f8d3b38bd8a996e44b067f0319607
Brandon Harrison <brandonharrisoncode@gmail.com>
2025-04-28 16:15:43 -0600

    Revert "Use arrayvec for moveordering"
    This reverts commit 747a85f1dfd7a73e6955cb0071027cfba3bce86d.
    
    Bench: 13749273

--- src/search/move_orderer.rs
+++ src/search/move_orderer.rs
@@ -1,14 +1,12 @@
 use std::sync::OnceLock;
 
-use crate::board::{
-    piece::{Color, Kind},
-    transposition_table::TRANSPOSITION_TABLE,
-    zkey::ZKey,
-    MoveList, Ply, MAX_PLY_PER_POSITION,
-};
+use crate::board::piece::Color;
+use crate::board::piece::Kind;
+use crate::board::transposition_table::TRANSPOSITION_TABLE;
+use crate::board::zkey::ZKey;
+use crate::board::Ply;
 
 mod scored_ply;
-use arrayvec::ArrayVec;
 use scored_ply::ScoredPly;
 
 use super::info::MAX_KILLERS;
@@ -45,18 +43,13 @@ impl ScoreBonus {
 }
 
 type MoveScore = u64;
-type ScoredMoveList = ArrayVec<ScoredPly, MAX_PLY_PER_POSITION>;
 
 pub struct MoveOrderer {
-    scored_moves: ScoredMoveList,
+    scored_moves: Vec<ScoredPly>,
     index: usize,
 }
 
-fn score_moves(
-    zkey: ZKey,
-    moves: MoveList,
-    killers: &[Option<Ply>; MAX_KILLERS],
-) -> ScoredMoveList {
+fn score_moves(zkey: ZKey, moves: &[Ply], killers: &[Option<Ply>; MAX_KILLERS]) -> Vec<ScoredPly> {
     let best_ply = TRANSPOSITION_TABLE
         .read()
         .expect("Transposition table is poisoned! Unable to read entry.")
@@ -64,8 +57,8 @@ fn score_moves(
         .map(|entry| entry.best_ply);
 
     moves
-        .into_iter()
-        .map(|ply| {
+        .iter()
+        .map(|&ply| {
             let score = score_move(ply, best_ply, killers);
             ScoredPly { ply, score }
         })
@@ -124,7 +117,7 @@ fn init_mvv_lva() -> [[MoveScore; VICTIMS_VALUE_ASCENDING.len()]; ATTACKERS_VALU
 }
 
 impl MoveOrderer {
-    pub fn new(moves: MoveList, zkey: ZKey, killers: &[Option<Ply>; MAX_KILLERS]) -> Self {
+    pub fn new(moves: &[Ply], zkey: ZKey, killers: &[Option<Ply>; MAX_KILLERS]) -> Self {
         let scored_moves = score_moves(zkey, moves, killers);
 
         Self {
